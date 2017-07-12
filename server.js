823eb35e5ce8afc465ad83462d0a0911612001c2
mireyasantiago <mireyasantiago105@gmail.com>
2017-07-12 16:29:00 -0500

    mi primer commit

---
+++ server.js
@@ -0,0 +1,29 @@
+/*se importa express y path
+var express = require('express');
+var path = require('path');
+var app = express();
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.listen(3000);
+
+app.use('/static', express.static(path.join(__dirname,'node_modules')));
+app.use('/static', express.static(path.join(__dirname,'assets')));
+
+*/
+
+
+/*se importa express y path para el ejercicio de manu*/
+var express = require('express');
+var app = express();
+
+app.use("/data", express.static(__dirname + "/data"));
+/*para buscar en ...*/
+
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname+'/index.html');
+});
+
+app.listen(3000);
