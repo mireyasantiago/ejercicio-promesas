e50ec76ffba6b5376a0e71457b01cac450bb811e
mireyasantiago <mireyasantiago105@gmail.com>
2017-07-13 12:57:04 -0500

    obtener los planetas

--- assets/js/ejercicio.js
+++ assets/js/ejercicio.js
@@ -1,1 +1,30 @@
 
+function getJSON(url) {
+    return new Promise(function(resolve,  reject) {
+        var ajax = new XMLHttpRequest();
+        var url = "data/earth-like-results.json";
+        ajax.open("GET", url);
+        ajax.send();
+        ajax.onreadystatechange = function(data) {
+            if (ajax.readyState == 4) {
+                resolve(JSON.parse(ajax.responseText));
+            };
+        };
+    });
+};
+
+
+/*para poder acceder a todos los planetas*/
+getJSON("data/earth-like-results.json")
+.then(function(mensaje){
+  mensaje.results.forEach(function(resultadoFinal){
+    getJSON(resultadoFinal).then(function(planetas){console.log(planetas)});
+  });
+})
+
+
+
+/*para acceder a un planeta en especifico
+getJSON("data/earth-like-results.json")
+.then(function(mensaje){return(getJSON(mensaje.results[0]))})
+.then(function(resultado){console.log(resultado)})*/
--- server.js
+++ server.js
@@ -17,9 +17,13 @@ app.use('/static', express.static(path.join(__dirname,'assets')));
 /*se importa express y path para el ejercicio de manu*/
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.use("/data", express.static(__dirname + "/data"));
 /*para buscar en ...*/
+app.use('/modules', express.static(path.join(__dirname,'node_modules')));
+app.use('/assets', express.static(path.join(__dirname,'assets')));
+
 
 
 app.get('/', function (req, res){
