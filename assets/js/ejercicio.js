1bb641c3c6bd3f289195620b0c3ae211a7ee46ef
sanfranciscodeasis <mireyasantiago105@gmail.com>
2017-07-13 23:58:42 -0500

    imprimiendo plantilla

--- assets/js/ejercicio.js
+++ assets/js/ejercicio.js
@@ -12,42 +12,10 @@ function getJSON(url) {
     });
 };
 
-
-/*para poder acceder a todos los planetas*/
-getJSON("data/earth-like-results.json")
-.then(function(response){
-
- var arreglObjeto=( response.results.map(function(url){
-    return getJSON(url)
-  }));
-  return Promise.all(arreglObjeto);
-})
-
-.then(function(arrayPromises){
-  console.log(arrayPromises)
-  arrayPromises.forEach(function(planeta){
-  console.log(planeta.pl_name)
-  var nombre= planeta.pl_name;
-  var radio= planeta.pl_rade;
-  var densidad= planeta.pl_dens;
- console.log(radio + " " + nombre + " " + densidad);
-
-
-  mostrarTotalPlanetas({
-    nombre: nombre,
-    radio: radio,
-    densidad: densidad
-  });
-
-
-  });
-});
-
-
 var plantilla=
 '<div class="col-md-offset-3 col-md-6">' +
   '<div class="thumbnail">' +
-    '<img src="assets/img/imagen.png" alt="#" class="img-circle">' +
+    '<img src="assets/img/planeta1.jpg" alt="#" class="img-circle">' +
     '<div class="caption">' +
       '<h3>__nombre__</h3>' +
       '<p>__radio__</p>' +
@@ -56,20 +24,90 @@ var plantilla=
   '</div>' +
 '</div>' ;
 
+/*
+var imagenPlaneta=[
+  {
+    "imagen": "assets/img/planeta1.jpg"
+  },
+  {
+    "imagen": "assets/img/planeta2.jpg"
+  },
+  {
+    "imagen": "assets/img/planeta3.jpg"
+  },
+  {
+    "imagen": "assets/img/planeta4.jpg"
+  },
+  {
+    "imagen": "assets/img/planeta5.jpg"
+  },
+  {
+    "imagen": "assets/img/planeta6.jpg"
+  }
 
-/*nombre del contenedor*/
-var contenedorPlanetas= document.getElementById('mostrandoPlanetas');
+];
+*/
 
-var mostrarTotalPlanetas= function(datos){
+/*para poder acceder a todos los planetas*/
+getJSON("data/earth-like-results.json")
 
-  var plantilaFinal= " ";
 
-    plantillaFinal += replace("__nombre__", datos.nombre)
-    .replace("__radio__", datos.radio)
-    .replace("__densidad__", datos.densidad)
-    contenedorPlanetas.innerHTML(plantillaFinal);
+.then(function(response){
 
-};
+ var arreglObjeto=( response.results.map(function(url){
+    return getJSON(url)
+  }));
+  return Promise.all(arreglObjeto);
+})
+
+
+
+
+
+
+
+.then(function(arrayPromises){
+  //console.log(arrayPromises)
+  /*nombre del contenedor*/
+  var contenedorPlanetas= document.getElementById('mostrandoPlanetas');
+  var plantillaFinal= " ";
+  arrayPromises.forEach(function(planeta){
+  //console.log(planeta.pl_name)
+
+// console.log(imagenPlaneta)
+  var nombre= planeta.pl_name;
+  var radio= planeta.pl_rade;
+  var densidad= planeta.pl_dens;
+
+
+
+
+  //console.log(radio + " " + nombre + " " + densidad);
+
+
+
+plantillaFinal += plantilla.replace("__nombre__",nombre)
+ .replace("__radio__",radio)
+ //.replace("__imagen__", imagenPlaneta)
+ .replace("__densidad__",densidad);
+  contenedorPlanetas.innerHTML += plantillaFinal;
+
+
+
+  });
+});
+
+/*
+var imagen= imagenPlaneta.map(function(obj){
+  return imagenPlaneta[0];
+});
+var imagenes= imagenPlaneta.resultado.map(function(obj){
+  return obj.imagen[0];
+});
+console.log(imagenes);
+*/
+
+
 
 
 
